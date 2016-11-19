91a5abc5e7b5de459584b7cdeba12a55d634cab1
Ben Holland <benjholla@gmail.com>
2016-11-19 09:53:51 -0600

    pushed some java specific analysis upstream

---
+++ com.ensoftcorp.open.java.commons/src/com/ensoftcorp/open/java/commons/subsystems/CryptoSubsystem.java
@@ -0,0 +1,33 @@
+package com.ensoftcorp.open.java.commons.subsystems;
+
+import com.ensoftcorp.open.commons.subsystems.Subsystem;
+
+public class CryptoSubsystem extends Subsystem {
+
+	public static final String TAG = "CRYPTO_SUBSYSTEM";
+
+	@Override
+	public String getName() {
+		return "Cryptography";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Cryptography libraries";
+	}
+
+	@Override
+	public String getTag() {
+		return TAG;
+	}
+
+	@Override
+	public String[] getParentTags() {
+		return new String[] { MathSubsystem.TAG, SecuritySubsystem.TAG };
+	}
+
+	@Override
+	public String[] getNamespaces() {
+		return new String[] { "javax.crypto", "javax.crypto.interfaces", "javax.crypto.spec" };
+	}
+}
---
+++ com.ensoftcorp.open.java.commons/src/com/ensoftcorp/open/java/commons/subsystems/MathSubsystem.java
@@ -0,0 +1,33 @@
+package com.ensoftcorp.open.java.commons.subsystems;
+
+import com.ensoftcorp.open.commons.subsystems.Subsystem;
+
+public class MathSubsystem extends Subsystem {
+
+	public static final String TAG = "MATH_SUBSYSTEM";
+
+	@Override
+	public String getName() {
+		return "Math";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Math libraries";
+	}
+
+	@Override
+	public String getTag() {
+		return TAG;
+	}
+
+	@Override
+	public String[] getParentTags() {
+		return new String[] { JavaCoreSubsystem.TAG };
+	}
+
+	@Override
+	public String[] getNamespaces() {
+		return new String[] { "java.math" };
+	}
+}
