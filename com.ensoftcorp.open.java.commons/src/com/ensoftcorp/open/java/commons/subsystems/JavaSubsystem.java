89651fed408d62e47d6037aee738d6b2246e9211
Ben Holland <benjholla@gmail.com>
2017-04-14 12:45:50 -0500

    bug fixes

--- com.ensoftcorp.open.java.commons/src/com/ensoftcorp/open/java/commons/subsystems/JavaSubsystem.java
+++ com.ensoftcorp.open.java.commons/src/com/ensoftcorp/open/java/commons/subsystems/JavaSubsystem.java
@@ -4,9 +4,11 @@ import com.ensoftcorp.open.commons.subsystems.Subsystem;
 
 public abstract class JavaSubsystem extends Subsystem {
 
+	public static String CATEGORY = "JAVA_SUBSYSTEM";
+	
 	@Override
 	public String getCategory() {
-		return "JAVA_SUBSYSTEM";
+		return CATEGORY;
 	}
 	
 	@Override
