7c3fcdade35e6506297b2bea9cef70722a1e0468
Ben Holland <benjholla@gmail.com>
2017-05-11 12:13:22 -0500

    added log4j coverage to logging subsystem

--- com.ensoftcorp.open.java.commons/src/com/ensoftcorp/open/java/commons/subsystems/LogSubsystem.java
+++ com.ensoftcorp.open.java.commons/src/com/ensoftcorp/open/java/commons/subsystems/LogSubsystem.java
@@ -26,7 +26,15 @@ public class LogSubsystem extends JavaSubsystem {
 
 	@Override
 	public String[] getNamespaces() {
-		return new String[] { "java.util.logging" };
+		return new String[] { 
+			"java.util.logging", // standard JDK logging apis
+			"org.apache.logging.log4j", // Public API for Log4j 2
+			"org.apache.logging.log4j.message", // Public Message Types used for Log4j 2
+			"org.apache.logging.log4j.simple", // Simple logging implementation
+			"org.apache.logging.log4j.spi", // Internal interfaces and classes to be used by authors of logging implementations or for internal use by API classes
+			"org.apache.logging.log4j.status", // Status API for Log4j 2
+			"org.apache.logging.log4j.util", // Internal utility classes for the Log4j 2 API
+			};
 	}
 
 }
